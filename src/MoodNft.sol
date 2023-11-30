47bcff9e5322478eafdea7eed3c593c2ea42a8b9
Mufassir Shah <mufassir.shah@7speaking.com>
2023-11-30 11:39:36 +0500

    Adding mood nft

---
+++ src/MoodNft.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenToUri;
+    string private s_sadSvgImgUri;
+    string private s_happySvgImgUri;
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory happySvgImgUri,
+        string memory sadSvgImgUri
+    ) ERC721("MoodNft", "MN") {
+        s_happySvgImgUri = happySvgImgUri;
+        s_sadSvgImgUri = sadSvgImgUri;
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        // s_tokenToUri[s_tokenCounter] = tokenUri;
+
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageUri;
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageUri = s_happySvgImgUri;
+        } else {
+            imageUri = s_sadSvgImgUri;
+        }
+
+        string memory tokenMetaData = Base64.encode(
+            bytes(
+                abi.encodePacked(
+                    (
+                        '{"name":"',
+                        name(),
+                        '","description":"An NFT reflecting the mood of the owner", "attributes:[{ "trait_type":"moodniess", "value":100}],"',
+                        '"image":',
+                        imageURI,
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        return string(abi.encodePacked(_baseURI(), tokenMetaData));
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
